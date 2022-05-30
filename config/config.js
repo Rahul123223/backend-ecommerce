36d8f96e1e8b58ac495a4caa95e1d6c107afc1c2
Rahul Garud <rvgarud25@gmail.com>
2022-05-30 23:17:43 +0530

    db

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const connect=()=>{
-    return mongoose.connect('mongodb+srv://rahul:garud@cluster0.mp3jd.mongodb.net/?retryWrites=true&w=majority')
+    return mongoose.connect('mongodb+srv://vaibhav:vaibhav@cluster0.bim0u.mongodb.net/ecommerce?retryWrites=true&w=majority')
 
 }
 module.exports=connect;
